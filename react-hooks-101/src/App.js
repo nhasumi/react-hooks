0afa843a6edd35cd7bcbc7d6fa7ab35db90c9661
nhasumi <nhasumi01@gmail.com>
2021-03-25 15:54:42 +0900

    create hellow react-hooks

--- react-hooks-101/src/App.js
+++ react-hooks-101/src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React , {useState} from 'react';
 
 
 const App = () => {
+  const [count, setCount] = useState(0)
+ 
+  const increment = () => { 
+    setCount(count +1 )
+  }
+
+  const decrement = () =>{
+    setCount(count-1)
+  }
+
+  const incremant2 = () => {
+    setCount ((previousCount) => previousCount +1)
+  }
+  const reset = () => setCount(0)
+  const x2 = () => setCount(count*2)
+
   return (
+    <React.Fragment>
+    <div>count : {count}</div>
+    <button onClick={increment}>+1</button>
+    <button onClick={decrement}>-1</button>
+    <button onClick={incremant2}>+1</button>
     <div>
-     This is a template for React app
+    <button onClick={reset}>RESET</button>
     </div>
+    <div>
+    <button onClick={x2}>x2</button>
+    </div>
+    </React.Fragment>
   );
 }
 
