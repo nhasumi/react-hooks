b07683cc5c2597fb32666866bbebcf8a65a309e1
nhasumi <nhasumi01@gmail.com>
2021-03-25 23:05:33 +0900

    create use-reducer-with-useReduers

--- react-hooks-101/src/reduces/index.js
+++ react-hooks-101/src/reduces/index.js
@@ -15,10 +15,10 @@
 
 
 const events = (state = [], action) => {
-
+    console.log(state)
     switch(action.type){
         case 'CREATE_EVENT':
-            const event = {titile: action.titile, body:action.body}
+            const event = {title: action.title, body:action.body}
             const length  = state.length
             const id = length === 0 ? 1 :state[length-1].id+1
             return [...state, {id, ...event}]
